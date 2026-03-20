969fc18982ff1fc6605173db27d2ed9b044b9aea
Palak-web <palak55544@gmail.com>
2026-03-20 22:29:34 +0530

    Practiced JS array methods: shift, splice, push (Company list modification)

--- practice_js/array.js
+++ practice_js/array.js
@@ -7,3 +7,20 @@ for (let val of marks) {
 }
 let avg = sum / marks.length;
 console.log(`Avg marks of the class = ${avg}`);
+
+// Qs2. Create an array to store companies --> "Bloomberg", "Microsofst", "Uber", "Google", "IBM", "Netflix"
+// a. remove the first company from the Array
+// b. remove Uber & Add Ola in its place
+// c. Add Amazon at the end
+
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"]
+console.log(companies);
+
+let comp2 = companies.shift();
+console.log("deleted item =",comp2);
+
+companies.splice(1, 1, "Ola");
+console.log(companies)
+
+companies.push("Amazon");
+console.log(companies)
