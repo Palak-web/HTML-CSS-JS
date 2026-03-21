b2b794011775efbb7ad293a8eb3ea37fc1856f5e
Palak-web <palak55544@gmail.com>
2026-03-21 23:45:21 +0530

    feat: JS Day 3 - loops  (for, while, do-while, for-of, for-in)

--- day_03_js/script.js
+++ day_03_js/script.js
@@ -1,14 +1,60 @@
-//Arrays in JS
-let marks = [ 92, 82, 88, 97, 90]
-console.log(marks);
-console.log(marks.length); //property
-let heroes = ["Spiderman", "hulk", "antman", "cpt of america" , "Shaktimaan", "Ironman", "Thor"]
-//Loops over an array
-console.log(heroes)
-for(let i=0; i< heroes.length ; i++) {
-    console.log(heroes[i])
+//for loop
+for (let count = 1; count<=5; count++){
+    console.log("Apna college"); // 5 execution
 }
-//for of
-for(let hero of heroes){
-    console.log(hero);
+
+//calculate sum of 1 to 5
+let sum= 0;
+let n = 33;
+for (let i= 1; i <=n; i++){
+    sum = sum + i;
+
+}
+console.log("sum = ",sum) 
+//print 1 to 5
+
+for (let i = 1; i <= 5; i++) {
+    console.log("i = ",i);
+}
+
+//while loop
+let j = 1;
+while (j <= 5) {
+    console.log("j =", j);
+    j++;
+}
+//do while 
+let k = 20;
+do {
+    console.log("Hello world!");
+    k++;
+} while (k <= 10);
+
+// Output: "Hello world!" (1 time)
+
+// for-of loop 
+let str = "Apna College";
+
+for(let p of str) { //iterator
+    console.log(p);
+}
+
+let str2 = "JavaScript"
+
+let size = 0;
+for (let a of str2){
+    console.log("a= ", a);
+    size++;
+}
+console.log("str2 size = ", size);
+//for in loop
+let student = {
+    name: "Kavita singh",
+    age:20,
+    cgpa: 7.7,
+    isPass: true
+};
+
+for (let key in student) {
+    console.log("key = ", key, "value =", student[key])
 }
