e163758636ed1ef55c8797a0016f829902ad7ca6
Palak-web <palak55544@gmail.com>
2026-03-28 23:40:33 +0530

    feat: JS Learning- DOM Tree Structure, Selecting access and Text/content access.

---
+++ learning_js/part_02_script.js
@@ -0,0 +1,16 @@
+// DOM MANIPULATION
+// HTML SE ELEMENT SELECT KARNA 
+// TEXT BADALNA
+// CSS BADALNA
+// ATTRIBUTE
+// EVENT LISTENERS 
+let abcd = document.getElementById("abcd");
+console.log(abcd);
+// let abc = document.getElementsByClassName("abc");
+// console.log(abc);
+let abc = document.querySelector("h4");
+console.dir(abc);
+let h1 = document.querySelector("h1");
+h1.innerHTML = "<i>hey hello</i>";
+// h1.hidden=true;
+console.dir(h1);
