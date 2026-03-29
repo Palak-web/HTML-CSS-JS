9f00eb19ae5563ed047944424287b7f645c96f35
Palak-web <palak55544@gmail.com>
2026-03-29 23:51:47 +0530

    feat: DOM - attribute manipulation (setAttribute, getAttribute, removeAttribute)

--- learning_js/part_02_script.js
+++ learning_js/part_02_script.js
@@ -14,3 +14,20 @@ let h1 = document.querySelector("h1");
 h1.innerHTML = "<i>hey hello</i>";
 // h1.hidden=true;
 console.dir(h1);
+//attribute Manipulation 
+
+// let a = document.querySelector("a");
+// // a.href= "https://www.google.com";
+// a.setAttribute("href", "https://www.google.com");
+// console.dir(a);
+
+let img = document.querySelector("img");
+img.setAttribute("src","https://thumbs.dreamstime.com/b/beautiful-scarlet-red-dahlia-flower-faded-blurry-blue-background-dark-art-moody-floral-toned-filters-retro-vintage-178241108.jpg");
+// img.setAttribute("src","")
+console.dir(img)
+
+let a = document.querySelector("a");
+console.log(a.getAttribute("href"));
+
+let u = document.getElementById("u");
+u.removeAttribute("href");
