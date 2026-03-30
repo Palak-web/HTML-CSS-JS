5d4843e592e6808a00f48f28f4cbb22de14fe029
Palak-web <palak55544@gmail.com>
2026-03-30 22:24:31 +0530

    Added dynamic DOM manipulation and basic styling via JS

--- learning_js/part_02_script.js
+++ learning_js/part_02_script.js
@@ -31,3 +31,23 @@ console.log(a.getAttribute("href"));
 
 let u = document.getElementById("u");
 u.removeAttribute("href");
+
+//Dynamic DOM manipulation
+let h3 = document.createElement("h3");
+h3.textContent = "Hello hey!";
+document.body.append(h3);  //prepend --> phle aa jayega
+// console.log("h3")
+let h11 = document.createElement("h1");
+h1.textContent = "Okay bye ";
+document.querySelector("div").append(h11);  //prepend --> phle aa jayega
+console.log("h3")
+
+//CSS via JS
+let h12 = document.querySelector("h1");
+h12.style.color = "red";
+h12.style.backgroundColor = "pink";
+h12.style.textTransform = "capitalize";
+console.dir(h12);
+let h4all = document.querySelector("h4");
+h4all.classList.add("abc")  //recommended  way
+h4all.classList.toggle("abc"); //reversing 
