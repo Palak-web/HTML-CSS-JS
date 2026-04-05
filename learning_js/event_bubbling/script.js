50f5438cb55f4fb434598478a3e7ca8e83be46ee
Palak-web <palak55544@gmail.com>
2026-04-05 21:38:23 +0530

    feat: event capturing and bubbling

--- learning_js/event_bubbling/script.js
+++ learning_js/event_bubbling/script.js
@@ -5,3 +5,26 @@ let ul = document.querySelector("ul");
 ul.addEventListener("click", function(dets){
     dets.target.classList.toggle("lt");
 });
+
+// jab bhi aao click karte ho ya koi bhi event raise karte ho to jo aapka event flow hai do phases me chalta hai
+// phase1: event top level element se niche ki taraf aayega 
+// phase 2: event raised element se parent ki taraf jayega
+
+let a = document.querySelector(".a");
+let b = document.querySelector(".b");
+let c = document.querySelector(".c");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function(dets){
+    console.log("button clicked");
+});
+
+c.addEventListener("click", function(dets){
+    console.log("c clicked");
+}, true);
+b.addEventListener("click", function(dets){
+    console.log("b clicked");
+});
+a.addEventListener("click", function(dets){
+    console.log("a clicked");
+}, true);
